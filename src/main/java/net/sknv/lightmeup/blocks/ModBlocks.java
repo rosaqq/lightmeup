9899dfda8827e48dfd805a237d45bbbf4d74acae
Tiago Rosa <tcrosa27@gmail.com>
2020-12-06 10:12:16 +0000

    link to camouflage block example

--- src/main/java/net/sknv/lightmeup/blocks/ModBlocks.java
+++ src/main/java/net/sknv/lightmeup/blocks/ModBlocks.java
@@ -11,5 +11,7 @@ public class ModBlocks {
 
     public static final List<Block> BLOCKS = new ArrayList<>();
 
+    // todo: make this block dynamically get he texture of the block it replaces in the overworld
+    // https://github.com/TheGreyGhost/MinecraftByExample/tree/1-12-2-final/src/main/java/minecraftbyexample/mbe04_block_dynamic_block_model1
     public static final Block LIGHT_BLOCK = new BlockBase("light_block", Material.ROCK);
 }
