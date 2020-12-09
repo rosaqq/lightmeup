f355614a15d73f17483e41fdbefd5d5b1ec83aa2
Tiago Rosa <tcrosa27@gmail.com>
2020-12-09 13:02:40 +0000

    method to return the corresponding ItemBlock

--- src/main/java/net/sknv/lightmeup/blocks/BlockBase.java
+++ src/main/java/net/sknv/lightmeup/blocks/BlockBase.java
@@ -10,18 +10,24 @@ import net.sknv.lightmeup.items.ModItems;
 
 public class BlockBase extends Block implements IHasModel {
 
+    public Item itemBlock;
+
     public BlockBase(String name, Material material) {
         super(material);
         setUnlocalizedName(name);
         setRegistryName(name);
-        setLightLevel(1f);
 
         ModBlocks.BLOCKS.add(this);
-        ModItems.ITEMS.add(new ItemBlock(this).setRegistryName(this.getRegistryName()));
+        itemBlock = new ItemBlock(this).setRegistryName(this.getRegistryName());
+        ModItems.ITEMS.add(itemBlock);
     }
 
     @Override
     public void registerModels() {
         LightMeUp.proxy.registerItemRenderer(Item.getItemFromBlock(this), 0, "inventory");
+    }
+
+    public Item getItemBlock() {
+        return this.itemBlock;
     }
 }
